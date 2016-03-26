cfe88682f2bfd368245ffad82c2280391a974ede
zhukening <kenju850915@gmail.com>
2016-03-26 20:06:19 +0800

    setAnt

--- appinventor-sources/appinventor/setAnt.bat
+++ appinventor-sources/appinventor/setAnt.bat
@@ -1,4 +1,4 @@
 set PATH=%BASEPATH%
 set ANT_HOME=G:\SB\SmartBook\apache-ant-1.9.6-bin\apache-ant-1.9.6
 set JAVA_HOME=C:\Program Files (x86)\Java\jdk1.7.0_45
-set PATH=%ANT_HOME%\bin;%JAVA_HOME%\bin;%PATH%
+set PATH=C:\Program Files\Git\cmd;%ANT_HOME%\bin;%JAVA_HOME%\bin;%PATH%
